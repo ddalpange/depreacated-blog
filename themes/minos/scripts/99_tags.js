c8f9aecc04d5731cee0d869a089dc2283a359d5b
yohan kim <yohan@knowru.com>
2018-12-07 15:07:10 +0900

    clean minos

--- themes/minos/scripts/99_tags.js
+++
@@ -1,7 +0,0 @@
-/**
- * Colored quote block
- */
-hexo.extend.tag.register('colorquote', function (args, content) {
-    var type =  args[0];
-    return '<blockquote class="colorquote ' + type + '">' + content + '</blockquote>';
-}, {ends: true});
